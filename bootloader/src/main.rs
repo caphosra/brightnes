48481d4bae00078feab55034e1e2410f20fc88f8
Akihisa Yagi <caphosra@gmail.com>
2024-09-30 13:23:36 +0900

    Implement quite simple bootloader

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -4,11 +4,12 @@
 use core::slice::from_raw_parts_mut;
 
 use log::info;
-use uefi::boot::{MemoryType, AllocateType};
+use uefi::boot::{AllocateType, MemoryType};
 use uefi::prelude::*;
-use uefi::proto::media::file::{File, FileInfo};
-use uefi::proto::media::file::FileAttribute;
-use uefi::proto::media::file::FileMode;
+use uefi::proto::media::file::{File, FileAttribute, FileInfo, FileMode};
+
+const KERNEL_ADDR: u64 = 0x100000;
+const FILE_INFO_SIZE: usize = 0x1000;
 
 #[entry]
 fn main() -> Status {
@@ -20,19 +21,42 @@ fn main() -> Status {
     let mut file_system = uefi::boot::get_image_file_system(handle).unwrap();
     let mut volume = file_system.open_volume().unwrap();
 
-    let mut kernel_file = volume.open(cstr16!("kernel"), FileMode::Read, FileAttribute::empty()).unwrap();
+    let mut kernel_file = volume
+        .open(cstr16!("kernel"), FileMode::Read, FileAttribute::empty())
+        .unwrap()
+        .into_regular_file()
+        .unwrap();
 
-    let file_info_size = 0x1000;
-    let info_buffer = uefi::boot::allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_DATA, file_info_size).unwrap();
-    let info_buffer = unsafe {
-        from_raw_parts_mut(info_buffer.as_ptr(), file_info_size)
-    };
+    let file_info_size = FILE_INFO_SIZE;
+    let info_buffer = uefi::boot::allocate_pages(
+        AllocateType::AnyPages,
+        MemoryType::LOADER_DATA,
+        file_info_size / 0x1000,
+    )
+    .unwrap();
+    let info_buffer = unsafe { from_raw_parts_mut(info_buffer.as_ptr(), file_info_size) };
     let info = kernel_file.get_info::<FileInfo>(info_buffer).unwrap();
 
     info!("Kernel file name: {}", info.file_name());
     info!("Kernel file size: {}", info.file_size());
 
+    let kernel_size = info.file_size() as usize;
+    let kernel_buffer = uefi::boot::allocate_pages(
+        AllocateType::Address(KERNEL_ADDR),
+        MemoryType::LOADER_DATA,
+        (kernel_size + 0xfff) / 0x1000,
+    )
+    .unwrap();
+    let kernel_buffer = unsafe { from_raw_parts_mut(kernel_buffer.as_ptr(), kernel_size) };
+
+    let read_size = kernel_file.read(kernel_buffer).unwrap();
+    info!("Read {} bytes to {:p}", read_size, kernel_buffer.as_ptr());
+
     boot::stall(10_000_000);
+
+    unsafe {
+        let _ = uefi::boot::exit_boot_services(MemoryType::BOOT_SERVICES_DATA);
+    }
 
     Status::SUCCESS
 }
