e8737da3df28d1248f2e07cfe923daddad8a229b
Akihisa Yagi <caphosra@gmail.com>
2025-08-17 18:08:46 +0900

    Load the frame buffer from the memory

--- kernel/src/frame_buffer.rs
+++ kernel/src/frame_buffer.rs
@@ -1,8 +1,14 @@
+use core::mem::transmute;
+
+use spin::RwLock;
+
 pub type PixelColor = u32;
+
+const FRAME_BUFFER_ADDR: u64 = 0x700000;
 
 #[repr(C)]
 pub struct FrameBuffer {
-    buffer: *mut u32,
+    buffer: u64,
     pub width: usize,
     pub height: usize,
     pub mode: PixelColorMode,
@@ -15,7 +21,28 @@ pub enum PixelColorMode {
     Bgr = 1,
 }
 
+pub static FRAME_BUFFER: RwLock<FrameBuffer> = RwLock::new(FrameBuffer {
+    buffer: 0,
+    width: 0,
+    height: 0,
+    mode: PixelColorMode::Rgb,
+});
+
 impl FrameBuffer {
+    pub fn init() {
+        let original: &mut FrameBuffer = unsafe {
+            transmute::<_, *mut Self>(FRAME_BUFFER_ADDR)
+                .as_mut()
+                .unwrap()
+        };
+
+        let mut frame_buffer = FRAME_BUFFER.write();
+        frame_buffer.buffer = original.buffer as u64;
+        frame_buffer.width = original.width;
+        frame_buffer.height = original.height;
+        frame_buffer.mode = original.mode;
+    }
+
     #[inline(always)]
     pub fn make_color(&self, r: u8, g: u8, b: u8) -> PixelColor {
         if cfg!(target_endian = "little") {
@@ -34,7 +61,8 @@ impl FrameBuffer {
     #[inline(always)]
     pub fn set_pixel(&mut self, x: usize, y: usize, color: PixelColor) {
         unsafe {
-            self.buffer.add(y * self.width + x).write(color);
+            let buffer: *mut u32 = transmute(self.buffer);
+            buffer.add(y * self.width + x).write(color);
         }
     }
 }
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,17 +4,20 @@
 use core::arch::asm;
 use core::panic::PanicInfo;
 
-use crate::frame_buffer::FrameBuffer;
+use crate::frame_buffer::{FrameBuffer, FRAME_BUFFER};
 
 #[no_mangle]
 #[inline(never)]
-pub extern "C" fn kernel_main(frame_buffer: *mut FrameBuffer) -> ! {
-    let mut frame_buffer = unsafe { frame_buffer.as_mut() }.unwrap();
+pub extern "C" fn kernel_main() -> ! {
+    FrameBuffer::init();
 
-    let black = frame_buffer.make_color(0x00, 0xFF, 0x00);
-    for y in 0..frame_buffer.height {
-        for x in 0..frame_buffer.width {
-            frame_buffer.set_pixel(x, y, black);
+    {
+        let mut frame_buffer = FRAME_BUFFER.write();
+        let black = frame_buffer.make_color(0x00, 0xFF, 0x00);
+        for y in 0..frame_buffer.height {
+            for x in 0..frame_buffer.width {
+                frame_buffer.set_pixel(x, y, black);
+            }
         }
     }
 
